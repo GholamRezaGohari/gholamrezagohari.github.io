5b10d7001d44acb5903e45f16de827f37c58f78a
GholamRezaGohari <GholamRezaGohari@users.noreply.github.com>
2026-01-30 10:45:47 +0000

    Deploying to gh-pages from @ GholamRezaGohari/gholamrezagohari.github.io@c3a726f8595e9b2471cd986b193f0671f9a9ea01 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "GholamRezaGohari/GholamRezaGohari.github.io",
+    "data-repo": "GholamRezaGohari/gholamrezagohari.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
